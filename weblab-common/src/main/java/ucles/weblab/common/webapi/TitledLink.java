280058cd5e1e89aeae8d8cdf4f8f6c4371875566
Sukhraj Rattan <rattan.s@cambridgeassessment.org.uk>
2016-01-29 15:21:42 +0000

    Added TitledLink as a seperate class as exampay and odt webapps use that. So it should belong in common.

---
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/TitledLink.java
@@ -0,0 +1,28 @@
+package ucles.weblab.common.webapi;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+/**
+ * A TitledLink is a Hateous link with the addition of a title. So in addition 
+ * to the rel and href attributes, there will also be a title attribute.
+ * 
+ */
+public class TitledLink extends Link {
+    
+    private String title;
+
+    public TitledLink(String href, String rel, String title) {
+        super(href, rel);
+        this.title = title;
+    }
+
+    public TitledLink(ControllerLinkBuilder linkBuilder, String rel, String title) {
+        this(linkBuilder.toString(), rel, title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
+}
