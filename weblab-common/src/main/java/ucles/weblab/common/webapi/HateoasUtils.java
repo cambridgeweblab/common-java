6a83ae19012759996b32c51f6cfc9dc33cd285a1
Sukhraj Rattan <rattan.s@cambridgeassessment.org.uk>
2016-06-08 17:22:27 +0100

    REA-55 Added some code from certstat HateoasLinks to register converters for enums so that they can be passed as RequestParam using spring mvc.

--- weblab-common/src/main/java/ucles/weblab/common/webapi/HateoasUtils.java
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/HateoasUtils.java
@@ -52,4 +52,46 @@ public class HateoasUtils {
         }
         return uriComponentsBuilder.build(false).toString();
     }
+    
+    /**
+     * Registers additional converters with Spring HATEOAS.
+     * Spring HATEOAS otherwise only registers default converters.
+     * @see <a href='http://stackoverflow.com/a/24081785'>Converter from @PathVariable DomainObject to String? (using ControllerLinkBuilder.methodOn)</a>
+     *
+     * @param converters the additional converters to register
+     */
+    public static void registerConverters(Converter<?, ?>... converters) {
+        try {
+            Class<?> clazz = Class.forName("org.springframework.hateoas.mvc.AnnotatedParametersParameterAccessor$BoundMethodParameter");
+            Field field = clazz.getDeclaredField("CONVERSION_SERVICE");
+            field.setAccessible(true);
+            DefaultFormattingConversionService service = (DefaultFormattingConversionService) field.get(null);
+            for (Converter<?, ?> converter : converters) {
+                service.addConverter(converter);
+            }
+        }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Registers additional converters with Spring HATEOAS, where the converters need a back reference to the conversion service.
+     * Spring HATEOAS otherwise only registers default converters.
+     * @see <a href='http://stackoverflow.com/a/24081785'>Converter from @PathVariable DomainObject to String? (using ControllerLinkBuilder.methodOn)</a>
+     *
+     * @param converterSource the additional converters to register
+     */
+    public static void registerConverter(Function<ConversionService, GenericConverter> converterSource) {
+        try {
+            Class<?> clazz = Class.forName("org.springframework.hateoas.mvc.AnnotatedParametersParameterAccessor$BoundMethodParameter");
+            Field field = clazz.getDeclaredField("CONVERSION_SERVICE");
+            field.setAccessible(true);
+            DefaultFormattingConversionService service = (DefaultFormattingConversionService) field.get(null);
+            service.addConverter(converterSource.apply(service));
+        }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
