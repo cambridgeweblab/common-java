fda71382a845a053f340d5a97301f9d36f1ce791
Gareth Boden <gareth.boden@tapina.com>
2016-07-31 23:44:47 +0100

    WEM-1190: Replace activiti-modeler controllers with our own implementation to workaround Spring issue #6519  https://github.com/spring-projects/spring-boot/issues/6519

--- weblab-common/src/main/java/ucles/weblab/common/webapi/MoreMediaTypes.java
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/MoreMediaTypes.java
@@ -8,7 +8,16 @@ import org.springframework.http.MediaType;
  * @since 07/05/15
  */
 public class MoreMediaTypes {
+    /**
+     * @deprecated Use {@link MediaType#APPLICATION_JSON_UTF8_VALUE} instead.
+     */
+    @Deprecated
     public static final String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
+
+    /**
+     * @deprecated Use {@link MediaType#APPLICATION_JSON_UTF8} instead.
+     */
+    @Deprecated
     public static final MediaType APPLICATION_JSON_UTF8;
 
     static {
