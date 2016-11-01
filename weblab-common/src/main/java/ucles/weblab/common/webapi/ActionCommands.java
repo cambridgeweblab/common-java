91c0dee39bc2c0d25cefb3dd50c33e7490cdd4f5
Gareth Boden <gareth.boden@tapina.com>
2016-11-01 00:06:44 +0000

    REA-136: Extend @ActionCommands to find them implicitly given just a workflow business key.

--- weblab-common/src/main/java/ucles/weblab/common/webapi/ActionCommands.java
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/ActionCommands.java
@@ -7,12 +7,18 @@ import java.lang.annotation.Target;
 
 /**
  * Defines actions which can be carried out on a resource.
- * Used along with {@link ucles.weblab.common.actions.webapi.ActionableResourceSupport} to populate a list of possible actions.
+ * Used along with {@link ucles.weblab.common.webapi.resource.ActionableResourceSupport} to populate a list of possible actions.
  *
  * @since 03/11/15
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface ActionCommands {
-    ActionCommand[] value();
+    ActionCommand[] value() default {};
+
+    /**
+     * Do not use this in conjunction with {@link ActionCommand#message()} as that will also include any pre-existing actions.
+     * @return Spring-EL expression identifying the business key to use to obtain actions from the workflow engine.
+     */
+    String businessKey() default "";
 }
