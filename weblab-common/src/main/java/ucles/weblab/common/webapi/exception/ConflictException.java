44b9633c74459f5c6d363fadb2da2c2a674ac7b1
sukhrajm <rattan.s@cambridgeassessment.org.uk>
2016-11-01 11:33:22 +0000

    WEM-1621 Handling errors in a better way so that that conflict exceptions can pass in a list of items that were conflicting.

--- weblab-common/src/main/java/ucles/weblab/common/webapi/exception/ConflictException.java
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/exception/ConflictException.java
@@ -1,6 +1,7 @@
 package ucles.weblab.common.webapi.exception;
 
 import java.io.Serializable;
+import java.util.List;
 import org.springframework.core.NestedRuntimeException;
 
 /**
@@ -9,19 +10,36 @@ import org.springframework.core.NestedRuntimeException;
  */
 public class ConflictException extends NestedRuntimeException {
     private final Serializable entityReference;
-
-    public ConflictException(String msg, Serializable entityReference) {
-        super(msg);
+    private String detail; 
+    private List<Object> conflictingItems;
+    
+    public ConflictException(String summary, Serializable entityReference) {
+        super(summary);
         this.entityReference = entityReference;
     }
-
+    
     public ConflictException(String msg, Serializable entityReference, Throwable cause) {
         super(msg, cause);
         this.entityReference = entityReference;
     }
+    
+    public ConflictException(String summary, String detail, List<Object> conflictingItems) {
+        super(summary);
+        this.detail = detail;
+        this.conflictingItems = conflictingItems;
+        this.entityReference = null;
+    }
 
     public Serializable getEntityReference() {
         return entityReference;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public List<Object> getConflictingItems() {
+        return conflictingItems;
     }
     
 }
