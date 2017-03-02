83ffe6c7dbf4f2f58c3df6db2f178675b27aca94
Neale Upstone <neale@nealeupstone.com>
2017-03-02 15:26:37 +0000

    Add @GetJson and @PostJson as clean shorthand for JSON RequestMappings

---
+++ weblab-common/src/main/java/ucles/weblab/common/webapi/GetJson.java
@@ -0,0 +1,57 @@
+package ucles.weblab.common.webapi;
+
+/*
+ * Copyright 2002-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * Shorthand for GET mapping which produces JSON.
+ *
+ * @author Sam Brannen (see See https://github.com/sbrannen/spring-composed/tree/master/src/main/java/org/springframework/composed/web/rest)
+ */
+@RequestMapping(method = GET, produces = APPLICATION_JSON_VALUE)
+@Target(METHOD)
+@Retention(RUNTIME)
+@Documented
+public @interface GetJson {
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "name")
+    String name() default "";
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] value() default {};
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] path() default {};
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "params")
+    String[] params() default {};
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "headers")
+    String[] headers() default {};
+
+}
