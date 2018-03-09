f06bef765552e89dd039fa329b0f4909799998ba
Neale Upstone <neale@nealeupstone.com>
2018-03-09 09:19:52 +0000

    Update to Jackson 2.9.4 and adjust TimeSerializer appropriately

--- weblab-common/src/main/java/ucles/weblab/common/serializer/TimeSerializer.java
+++ weblab-common/src/main/java/ucles/weblab/common/serializer/TimeSerializer.java
@@ -1,5 +1,6 @@
 package ucles.weblab.common.serializer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
@@ -9,25 +10,21 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * A custom time serializer that will serialize a time using the hours and minutes only. 
- * 
+ * A custom time serializer that will serialize a time using the hours and minutes only.
+ *
  * @author Sukhraj
  */
-public class TimeSerializer extends LocalTimeSerializer{
-    
+public class TimeSerializer extends LocalTimeSerializer {
+
     private static final String TIME_PATTERN = "HH:mm";
-        
+
     public TimeSerializer() {
         super(DateTimeFormatter.ofPattern(TIME_PATTERN));
     }
-    
+
     /**
      * A method that will convert a Java time object to a JSON string only showing
-     * the hours and minutes. 
-     * @param value - the Java LocalTime object to serialize
-     * @param g 
-     * @param provider
-     * @throws IOException 
+     * the hours and minutes.
      */
     @Override
     public void serialize(LocalTime value, JsonGenerator g, SerializerProvider provider) throws IOException {
@@ -46,18 +43,12 @@ public class TimeSerializer extends LocalTimeSerializer{
     }
 
     /**
-     * Override this method to return the format which just have hours and minutes. 
-     * 
-     * @param useTimestamp
-     * @param dtf
-     * @return 
+     * Override this method to return the format which just have hours and minutes.
      */
     @Override
-    protected LocalTimeSerializer withFormat(Boolean useTimestamp, DateTimeFormatter dtf) {
+    protected LocalTimeSerializer withFormat(Boolean useTimestamp, DateTimeFormatter dtf, JsonFormat.Shape shape) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_PATTERN);
         return new LocalTimeSerializer(formatter);
     }
-
-    
 
 }
